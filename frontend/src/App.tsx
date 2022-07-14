972f9474aadd172a54e269c53e73e6e61a3bdfa8
Felipe Schneider <felipeschneider2811@gmail.com>
2022-07-14 18:15:39 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from "./components/notificationButton";
 
 function App() {
   return (
-    <h1>Olá mundo</h1>
-  )
+    <>
+      <h1>Olá mundo</h1>
+      <NotificationButton />
+    </>
+  );
 }
 
-export default App
+export default App;
