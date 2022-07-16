e787e6aa77dc75ea20fce73c10db828b7c42e097
Felipe Schneider <felipeschneider2811@gmail.com>
2022-07-16 11:16:10 -0300

    implementing axios, useEffect first request

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -8,7 +8,7 @@ function Header() {
       <img src={logo} alt="DSMeta" />
       <h1>DSMeta</h1>
       <p>
-        Desenvolvido por
+        Desenvolvido por 
         <a href="https://www.linkedin/in/felpschneider">Felipe Schneider</a>
       </p>
     </div>
