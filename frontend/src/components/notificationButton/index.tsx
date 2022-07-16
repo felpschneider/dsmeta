b3f477ab7c7d663eca08c9fc166979ed191df277
Felipe Schneider <felipeschneider2811@gmail.com>
2022-07-16 11:47:21 -0300

    Send notification

--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -1,10 +1,22 @@
+import axios from "axios";
 import icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
 
 import "./styles.css";
 
-function NotificationButton() {
+type Props = {
+  saleID: number;
+};
+
+function handleClick(Id: number) {
+  axios(`${BASE_URL}/sales/${Id}/notification`).then((Response) => {
+    console.log("Sucesso!");
+  });
+}
+
+function NotificationButton({ saleID }: Props) {
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleID)}>
       <img src={icon} alt="Notificar" />
     </div>
   );
